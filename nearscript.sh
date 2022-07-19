9c5078467b2b07b59b21e2228344e6be7d61d15f
doma2k <domakyrylo@gmail.com>
2022-07-19 15:28:44 +0200

    lala

--- nearscript.sh
+++ nearscript.sh
@@ -22,47 +22,44 @@ fi
 
 if [ ! $NEAR_ENV ]; then
 read -p "Enter chain name: " NEAR_ENV
-echo 'export NEAR_ENV='\"${NEAR_ENV}\" >> $HOME/.bash_profile
+echo 'export NEAR_ENV='\"${NEAR_ENV}\" >> $HOME/.bashrc
 fi
 echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
 echo "export WORKSPACE=\"$HOME/.near\"" >>$HOME/.bash_profile
 . $HOME/.bash_profile
 
-#install dependenies
+
+sudo apt update && sudo apt upgrade -y
+
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -  
+sudo apt install build-essential nodejs -y
+PATH="$PATH"
+
+sudo npm install -g near-cli -y
 sudo apt install -y git binutils-dev libcurl4-openssl-dev zlib1g-dev libdw-dev libiberty-dev cmake gcc g++ python docker.io protobuf-compiler libssl-dev pkg-config clang llvm cargo -y
-apt update && apt install git sudo unzip wget -y
 sudo apt install python3-pip -y
+
 USER_BASE_BIN=$(python3 -m site --user-base)/bin
 export PATH="$USER_BASE_BIN:$PATH"
+
 sudo apt install clang build-essential make -y
-sudo apt install git -y
-
-#install docker and docker-compose
-if exists docker; then
-echo 'docker already installed'
-else
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-fi
-
-#install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
-#install near
+cd $HOME
 git clone https://github.com/near/nearcore
 cd nearcore
 git fetch
-git checkout latest 
+
+git checkout 8448ad1ebf27731a43397686103aa5277e7f2fcf
 cargo build -p neard --release --features shardnet
-./target/release/neard --home ~/.near init --chain-id shardnet --download-genesis
+
 rm ~/.near/config.json
 wget -O ~/.near/config.json https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/shardnet/config.json
 
-#create systemd    
+cd ~/.near
+wget https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/shardnet/genesis.json
+
 sudo tee <<EOF >/dev/null $HOME/neard.service
 [Unit]
 Description=NEARd Daemon Service
@@ -95,12 +92,3 @@ else
   echo -e "Your Near node \e[31mwas not installed correctly\e[39m, please reinstall."
 fi
 . $HOME/.bash_profile
-
-# monitoring install
-IPADDR=$(curl ifconfig.me)
-cd $HOME
-sudo git clone https://github.com/doma2k/monNear.git
-cd monNear
-sudo docker-compose up -d
-
-
